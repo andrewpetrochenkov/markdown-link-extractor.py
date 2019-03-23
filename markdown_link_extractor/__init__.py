6053622582fce9ca15fe383811e1f69a9d97cdb0
looking-for-a-job <russianidiot.github@gmail.com>
2019-03-23 18:53:38 +0300

    ^markdown_link_extractor/__init__.py,setup.cfg

--- markdown_link_extractor/__init__.py
+++ markdown_link_extractor/__init__.py
@@ -8,4 +8,6 @@ import re
 def getlinks(string):
     """return a list with markdown links"""
     html = markdown.markdown(string, output_format='html')
-    return list(set(re.findall(r'href=[\'"]?([^\'" >]+)', html)))
+    links = list(set(re.findall(r'href=[\'"]?([^\'" >]+)', html)))
+    links = list(filter(lambda l: l[0] != "{", links))
+    return links
